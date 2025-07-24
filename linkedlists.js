9e02d765a31a5e4f651b19fbdea45707a5c60f80
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-07-23 19:00:24 -0600

    chore: initial commit

---
+++ linkedlists.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+}
